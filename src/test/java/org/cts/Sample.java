4694aea6cf3cb6fd057c7db2ede374e60ed71a74
srinivasansathis <srinivasanksthala@gmail.com>
2025-03-29 13:39:03 +0530

    First time push

---
+++ src/test/java/org/cts/Sample.java
@@ -0,0 +1,17 @@
+package org.cts;
+
+public class Sample {
+	 public void method1() {
+		 System.out.println("Lionel Messi");
+	 }
+	 public void method2() {
+		 System.out.println("Cristiano Ronaldo");
+		 
+	 }
+	 public void method3() {
+		 System.out.println("Neymar");
+		 
+	 }
+	
+
+}
